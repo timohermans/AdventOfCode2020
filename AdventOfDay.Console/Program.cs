d91ed5f2ca839183bbde9121310df114ffa72bf6
Timo Hermans <tmhermans@gmail.com>
2020-12-03 20:48:53 +0100

    Add a Console project for visuals

---
+++ AdventOfDay.Console/Program.cs
@@ -0,0 +1,24 @@
+﻿using AdventOfCode.Day1;
+using AdventOfCode.Day1.Implementation;
+using System;
+
+namespace AdventOfDay.Consoled
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var report = Data.Report;
+
+            var finder = new MatchFinder
+            {
+                TotalToMatch = 2020,
+                NumberOfElementsToMatch = 3
+            };
+
+            var result = finder.Find(report);
+            Console.WriteLine("Done");
+            Console.ReadLine();
+        }
+    }
+}
