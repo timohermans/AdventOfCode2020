a76b6fe10b67205340565cf99c13d9090335ba51
Timo Hermans <tmhermans@gmail.com>
2020-12-04 08:23:53 +0100

    Refactor passport property names

--- AdventOfCode.Day4/Implementation/Passport.cs
+++ AdventOfCode.Day4/Implementation/Passport.cs
@@ -8,27 +8,27 @@ namespace AdventOfCode.Day4.Implementation
 {
     public class Passport
     {
-        public string Ecl { get; private set; }
-        public string Pid { get; private set; }
-        public int Eyr { get; private set; }
-        public string Hcl { get; private set; }
-        public int Byr { get; private set; }
-        public int Iyr { get; private set; }
-        public int Hgt { get; private set; }
-        public string Cid { get; private set; }
+        public string EyeColor { get; private set; }
+        public string PersonalId { get; private set; }
+        public int ExpirationYear { get; private set; }
+        public string HairColor { get; private set; }
+        public int BirthYear { get; private set; }
+        public int IssuerYear { get; private set; }
+        public int Height { get; private set; }
+        public string CountryId { get; private set; }
 
         public Passport(string input)
         {
             var parts = string.Join(' ', input.Split("\r\n")).Split(' ').ToDictionary(s => s.Split(':')[0], s => s.Split(':')[1]);
 
-            SetEyeColor(parts.GetValueOrDefault("ecl") ?? throw new ArgumentNullException(nameof(Ecl)));
-            SetPid(parts.GetValueOrDefault("pid") ?? throw new ArgumentNullException(nameof(Pid)));
-            Eyr = GetValidNumberRange("eyr", parts, 2020, 2030);
-            SetHairColor(parts.GetValueOrDefault("hcl") ?? throw new ArgumentNullException(nameof(Hcl)));
-            Byr = GetValidNumberRange("byr", parts, 1920, 2002);
-            Iyr = GetValidNumberRange("iyr", parts, 2010, 2020);
-            SetHeight(parts.GetValueOrDefault("hgt") ?? throw new ArgumentNullException(nameof(Hgt)));
-            Cid = parts.GetValueOrDefault("cid");
+            SetEyeColor(parts.GetValueOrDefault("ecl") ?? throw new ArgumentNullException(nameof(EyeColor)));
+            SetPid(parts.GetValueOrDefault("pid") ?? throw new ArgumentNullException(nameof(PersonalId)));
+            ExpirationYear = GetValidNumberRange("eyr", parts, 2020, 2030);
+            SetHairColor(parts.GetValueOrDefault("hcl") ?? throw new ArgumentNullException(nameof(HairColor)));
+            BirthYear = GetValidNumberRange("byr", parts, 1920, 2002);
+            IssuerYear = GetValidNumberRange("iyr", parts, 2010, 2020);
+            SetHeight(parts.GetValueOrDefault("hgt") ?? throw new ArgumentNullException(nameof(Height)));
+            CountryId = parts.GetValueOrDefault("cid");
         }
 
         private void SetPid(string pid)
@@ -37,7 +37,7 @@ namespace AdventOfCode.Day4.Implementation
 
             if (m.Success)
             {
-                Pid = pid;
+                PersonalId = pid;
                 return;
             }
 
@@ -50,7 +50,7 @@ namespace AdventOfCode.Day4.Implementation
 
             if (validColors.Contains(ecl))
             {
-                Ecl = ecl;
+                EyeColor = ecl;
                 return;
             }
 
@@ -65,7 +65,7 @@ namespace AdventOfCode.Day4.Implementation
 
             if (m.Success)
             {
-                Hcl = hclString;
+                HairColor = hclString;
                 return;
             }
 
@@ -79,13 +79,13 @@ namespace AdventOfCode.Day4.Implementation
 
             if (heightMetric == "cm" && heightNumber >= 150 && heightNumber <= 193)
             {
-                Hgt = heightNumber;
+                Height = heightNumber;
                 return;
             }
 
             if (heightMetric == "in" && heightNumber >= 59 && heightNumber <= 76)
             {
-                Hgt = heightNumber;
+                Height = heightNumber;
                 return;
             }
 
@@ -109,7 +109,7 @@ namespace AdventOfCode.Day4.Implementation
 
         private int GetValidNumberRange(string key, Dictionary<string, string> parts, int min, int max)
         {
-            var yearString = parts.GetValueOrDefault(key) ?? throw new ValidationException(nameof(Byr));
+            var yearString = parts.GetValueOrDefault(key) ?? throw new ValidationException(nameof(BirthYear));
 
             int year;
 
